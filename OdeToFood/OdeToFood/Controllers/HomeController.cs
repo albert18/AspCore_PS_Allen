c85a5ed963d476061edec22faa37421316ce90a3
albert18 <caps18@hotmail.com>
2018-05-20 10:20:31 -0400

    Added Controller

---
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+namespace OdeToFood.bin.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from the home controller!";
+        }
+    }
+}
